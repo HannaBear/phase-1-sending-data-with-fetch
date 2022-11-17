dad698bda77591183a332e2f4ee972cd050ef841
Hanna <marmalademarmoset@gmail.com>
2022-11-16 20:25:05 -0800

    commit tree aede4346391fd33033a7ffbe39e622924efefe96

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+
+
+return fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json',
+    },
+    
+    body:JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com",
+    
+
+    }),
+    })
+    .then (res => res.json())
+    .then (data => document.body.append(data.id))
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.body.append(error.message);
+      });
+
+}
